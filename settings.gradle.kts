49fff063634c1972a4fc22fd4a002ff50777e400
Syfe <22247810+ItsSyfe@users.noreply.github.com>
2026-09-06 20:18:40 +0300

    chore: add loom back compat and bump stonecutter to 0.9.8

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,12 +4,19 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         maven("https://maven.fabricmc.net/")
+        maven("https://maven.kikugie.dev/releases") { name = "KikuGie Releases" }
         maven("https://maven.kikugie.dev/snapshots") { name = "KikuGie Snapshots" }
     }
 }
 
 plugins {
-    id("dev.kikugie.stonecutter") version "0.9.6"
+    id("dev.kikugie.stonecutter") version "0.9.8"
+
+    id("dev.kikugie.loom-back-compat") version "0.4.2"
+
+    // stonecutter template says sometimes gradle doesn't work without this?
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+
 }
 
 stonecutter {
